bcabda21cf66a39c256cf8eb9164e195ee352eaa
rangaraju <rangaraju29139>
2017-12-20 10:12:13 +0530

    day 2 commit

--- formhandle1.js
+++ formhandle1.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mongo =reui
 var app = express();
 /*
       app.use(express.static(__dirname+'/',{login:'/login.html'}));
@@ -9,6 +10,7 @@ res.sendFile(__dirname +'/login.html');
 app.get('/insert',function(req,res){
 var username = req.query.username;
 var password = req.query.password;
+
 console.log('the username of the user is : '+username);
 console.log('the password of the user is:'+password);
 });
